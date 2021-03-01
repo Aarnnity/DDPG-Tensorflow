d1644c0787fef3dc4a757fd744f326489243395a
Aarn <66675156+Aarnnity@users.noreply.github.com>
2021-03-01 17:09:22 +0800

    Update ReplayBuffer.py

--- ReplayBuffer.py
+++ ReplayBuffer.py
@@ -4,12 +4,12 @@ import random
 class ReplayBuffer(object):
 
     def __init__(self, buffer_size):
-        self.buffer_size = buffer_size
+        self.buffer_size = buffer_size              #buffer_size=10000 
         self.num_experiences = 0
         self.buffer = deque()
-        self.mean_reward = 0.0
+        self.mean_reward = 0.0    #1、添加了这个
 
-    def getBatch(self, batch_size):
+    def getBatch(self, batch_size):                #when the agent is trainning, and need samples.
         # Randomly sample batch_size examples
         if self.num_experiences < batch_size:
             return random.sample(self.buffer, self.num_experiences)
@@ -27,15 +27,14 @@ class ReplayBuffer(object):
         else:
             self.buffer.popleft()
             self.buffer.append(experience)
-        self.mean_reward = ( (self.mean_reward * (self.num_experiences-1)) + reward) / float( self.num_experiences )
-
+        self.mean_reward = ( (self.mean_reward * (self.num_experiences-1)) + reward) / float( self.num_experiences ) #2、every step's average reward: sum of reward / sum of steps.
 
     def count(self):
         # if buffer is full, return buffer size
         # otherwise, return experience counter
         return self.num_experiences
 
-    def getMeanReward(self):
+    def getMeanReward(self):                                                #3、acquire mean_reward.
         # if buffer is full, return buffer size
         # otherwise, return experience counter
         return self.mean_reward
